92efada4ef941165abcb2d6b4833227f4ec981c2
takeuchi <g.takeuchi@gmail.com>
2012-05-02 10:38:27 -0300

    Added some default code templates

--- plugin/src/objective/Startup.java
+++ plugin/src/objective/Startup.java
@@ -1,10 +1,24 @@
 package objective;
 
+import org.eclipse.jdt.internal.ui.JavaPlugin;
+import org.eclipse.jface.text.templates.Template;
+import org.eclipse.jface.text.templates.persistence.TemplateStore;
 import org.eclipse.ui.IStartup;
 
+@SuppressWarnings("restriction")
 public class Startup implements IStartup {
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void earlyStartup() {
+		TemplateStore codeTemplateStore = JavaPlugin.getDefault().getCodeTemplateStore();
+		Template methodBody = codeTemplateStore.findTemplate("methodbody");
+		methodBody.setPattern("throw new RuntimeException(\"NOT IMPLEMENTED\");");
+		
+		Template constructorBody = codeTemplateStore.findTemplate("constructorbody");
+		constructorBody.setPattern("throw new RuntimeException(\"NOT IMPLEMENTED\");");
+		
+		Template catchBlock = codeTemplateStore.findTemplate("catchblock");
+		catchBlock.setPattern("throw new RuntimeException(${exception_var});");
 	}
 }
