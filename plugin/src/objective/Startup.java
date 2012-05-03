638dcd71477725b00d82516ba16f542c97ec0837
gtakeuchi <gabriel.takeuchi@objective.com.br>
2012-05-03 18:00:09 -0300

    Added a feature to allow navigating to a specific class method from a string sent to a socket port

--- plugin/src/objective/Startup.java
+++ plugin/src/objective/Startup.java
@@ -1,11 +1,14 @@
 package objective;
 
+import objective.ng.GotoMethodServer;
+
 import org.eclipse.core.runtime.preferences.ConfigurationScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jface.text.templates.Template;
 import org.eclipse.jface.text.templates.persistence.TemplateStore;
 import org.eclipse.ui.IStartup;
+import org.osgi.service.prefs.BackingStoreException;
 
 @SuppressWarnings("restriction")
 public class Startup implements IStartup {
@@ -25,5 +28,17 @@ public class Startup implements IStartup {
 
 		IEclipsePreferences preferences = ConfigurationScope.INSTANCE.getNode("org.eclipse.core.runtime");
 		preferences.putBoolean("escapeStrings", true);
+		try {
+			preferences.flush();
+		} catch (BackingStoreException e) {
+			throw new RuntimeException(e);
+		}
+		
+		startGotoMethodServer();
+	}
+
+	private void startGotoMethodServer() {
+		new GotoMethodServer().start();
+		
 	}
 }
